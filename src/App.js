86d09bc08b99601cfe36ef06820540ba93c99eec
Ariel <arielpiresdealmeida@gmail.com>
2022-02-11 19:58:18 -0300

    remove App.styles.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { Wrapper } from './App.styles'
 import Body from './components/Body/Body'
 import Header from './components/Header/Header'
 
 function App() {
   return (
-    <Wrapper>
+    <>
       <Header />
       <Body />
-    </Wrapper>
+    </>
   )
 }
 
