1ab2b8bc9ccc84eddc7178d99ded6714a77fc32d
Ariel <arielpiresdealmeida@gmail.com>
2022-02-13 18:15:48 -0300

    feature: responsive body

--- src/components/Body/Body.styles.js
+++ src/components/Body/Body.styles.js
@@ -12,15 +12,19 @@ export const Wrapper = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 16vh;
+    padding: 5vh;
     margin-top: -10vh;
     margin-left: 10vw;
     z-index: 10;
     background-color: white;
     width: 80%;
-    
+
     div {
       display: flex;
+    }
+
+    @media (min-width: 842px) {
+      padding: 16vh;
     }
   `}
 `
@@ -28,9 +32,10 @@ export const Wrapper = styled.section`
 export const OptionsWrapper = styled.section`
   ${({ theme }) => css`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     padding-top: 5vh;
-    flex-wrap: wrap;
+    flex-wrap: nowrap;
+    align-items: initial;
 
     div {
       display: flex;
@@ -39,7 +44,7 @@ export const OptionsWrapper = styled.section`
       padding: 80px;
       border-radius: 32px;
       background-image: linear-gradient(#ffffff, #ebebeb);
-      margin-right: 5vh;
+      margin-right: 0;
       justify-content: space-between;
       gap: 20px;
 
@@ -48,6 +53,14 @@ export const OptionsWrapper = styled.section`
         color: #4b4b4b;
       }
     }
-    
+
+    @media (min-width: 842px) {
+      flex-direction: row;
+      flex-wrap: wrap;
+
+      div {
+        margin-right: 5vh;
+      }
+    }
   `}
 `
